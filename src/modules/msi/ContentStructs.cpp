af4613a42f8529576238257cfd397cf61bb570d4
Ariman <ariman@inbox.ru>
2009-11-17 16:07:39 +0000

    MSI: Append duplicate resolver int values to file name part to keep extension sorting.

--- src/modules/msi/ContentStructs.cpp
+++ src/modules/msi/ContentStructs.cpp
@@ -53,10 +53,22 @@ BasicNode::~BasicNode()
 
 void BasicNode::AppendNumberToName(int val)
 {
+	if (val > 1000) return; // Safety check
+	
 	size_t nNewLen = wcslen(TargetName) + 5;  // 1 for comma, 1 for 0-term, 3 for digits
 	wchar_t *newName = (wchar_t *) malloc(nNewLen * sizeof(wchar_t));
-	swprintf_s(newName, nNewLen, L"%s,%d", TargetName, val);
 
+	wchar_t* extPos = wcsrchr(TargetName, L'.');
+	if (extPos)
+	{
+		*extPos = 0;
+		swprintf_s(newName, nNewLen, L"%s,%d.%s", TargetName, val, extPos + 1);
+	}
+	else
+	{
+		swprintf_s(newName, nNewLen, L"%s,%d", TargetName, val);
+	}
+	
 	KILLSTR(TargetName);
 	TargetName = newName;
 }
