822fe170f7598d22b9c0de02bdfd70ccf6404681
Ariman <ariman@inbox.ru>
2010-03-30 09:45:02 +0000

    Cosmetics

--- src/modules/udfimg/udfimg.cpp
+++ src/modules/udfimg/udfimg.cpp
@@ -127,7 +127,6 @@ int MODULE_EXPORT ExtractItem(INT_PTR *storage, ExtractOperationParams params)
 
 	if (!item.IsDir())
 	{
-		ProgressContext* pctx = (ProgressContext*) params.callbacks.signalContext;
 		int res = storageRec->arc.DumpFileContent(file.ItemIndex, ref.FileIndex, params.dest_path, &(params.callbacks));
 
 		return res;
