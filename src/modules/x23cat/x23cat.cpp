1cb8d16980bed01924b6a7c71ae49e31a10ac947
Ariman <ariman@inbox.ru>
2010-05-11 17:03:44 +0000

    Added some empty projects for new formats

---
+++ src/modules/x23cat/x23cat.cpp
@@ -0,0 +1,30 @@
+// isoimg.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "ModuleDef.h"
+
+
+int MODULE_EXPORT LoadSubModule(const wchar_t* settings)
+{
+	return TRUE;
+}
+
+int MODULE_EXPORT OpenStorage(const wchar_t *path, INT_PTR **storage, StorageGeneralInfo* info)
+{
+	return FALSE;
+}
+
+void MODULE_EXPORT CloseStorage(INT_PTR *storage)
+{
+}
+
+int MODULE_EXPORT GetStorageItem(INT_PTR* storage, int item_index, LPWIN32_FIND_DATAW item_data, wchar_t* item_path, size_t path_size)
+{
+	return GET_ITEM_NOMOREITEMS;
+}
+
+int MODULE_EXPORT ExtractItem(INT_PTR *storage, ExtractOperationParams params)
+{
+	return SER_ERROR_SYSTEM;
+}
