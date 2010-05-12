6f86612c510a10f20a9c6f20871fbc688b7e7995
Ariman <ariman@inbox.ru>
2010-05-12 19:15:10 +0000

    VP: Extraction progress.

--- src/modules/vp/vp_file.cpp
+++ src/modules/vp/vp_file.cpp
@@ -168,7 +168,8 @@ int CVPFile::ExtractSingleFile( int index, const wchar_t* destPath, const Extrac
 	if (hOutput == INVALID_HANDLE_VALUE) return SER_ERROR_WRITE;
 
 	int ret = SER_SUCCESS;
-
+	ProgressContext* pctx = (ProgressContext*) epc->signalContext;
+	
 	int bufSize = 16 * 1024;
 	char* buf = (char *) malloc(bufSize);
 
@@ -191,8 +192,21 @@ int CVPFile::ExtractSingleFile( int index, const wchar_t* destPath, const Extrac
 		}
 
 		nBytesLeft -= dwNumRead;
+		pctx->nProcessedBytes += dwNumRead;
+		
+		int nProgressVal =  ((__int64)(frec.size - nBytesLeft) * 100) / frec.size;
+		if (nProgressVal != pctx->nCurrentFileProgress)
+		{
+			pctx->nCurrentFileProgress = nProgressVal;
+
+			if (!epc->FileProgress(epc->signalContext))
+			{
+				ret = SER_USERABORT;
+				break;
+			}
+		}
 	}
-	
+
 	free(buf);
 	CloseHandle(hOutput);
 	return ret;
