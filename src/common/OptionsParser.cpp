c50c06e9eb2f8a2959a133b56f1cd74ebd16ac82
Ariman <ariman@inbox.ru>
2010-08-31 14:04:47 +0000

    Fancy wrapper class for registry settings reading.

--- src/common/OptionsParser.cpp
+++ src/common/OptionsParser.cpp
@@ -2,6 +2,20 @@
 #include "OptionsParser.h"
 
 #define SECTION_BUF_SIZE 1024
+#define SETTINGS_KEY_REGISTRY L"Observer"
+
+static wstring ConvertString(const char* Input)
+{
+	int nNumWChars = MultiByteToWideChar(CP_ACP, 0, Input, -1, NULL, NULL);
+	wchar_t* buf = new wchar_t[nNumWChars + 1];
+	MultiByteToWideChar(CP_ACP, 0, Input, -1, buf, nNumWChars + 1);
+	wstring strResult(buf);
+	delete [] buf;
+
+	return strResult;
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 bool ParseOptions(const wchar_t* wszConfigFile, const wchar_t* wszSectionName, OptionsList &opts)
 {
@@ -74,6 +88,22 @@ wstring OptionsList::GetValueAsString( const wchar_t* Key, const wchar_t* Defaul
 	return Default;
 }
 
+
+string OptionsList::GetValueAsAnsiString( const wchar_t* Key, const char* Default )
+{
+	wstring strBuf = GetValueAsString(Key, L"");
+	if (strBuf.length() > 0)
+	{
+		int nSize = WideCharToMultiByte(CP_ACP, 0, strBuf.c_str(), -1, NULL, 0, NULL, NULL);
+		char* tmpBuf = (char*) malloc(nSize + 1);
+		memset(tmpBuf, 0, nSize + 1);
+		WideCharToMultiByte(CP_ACP, 0, strBuf.c_str(), -1, tmpBuf, nSize + 1, NULL, NULL);
+		return tmpBuf;
+	}
+	return Default;
+}
+
+
 int OptionsList::GetValueAsInt( const wchar_t* Key, int Default )
 {
 	wstring StrVal = GetValueAsString(Key, L"");
@@ -94,4 +124,105 @@ bool OptionsList::GetValueAsBool( const wchar_t* Key, bool Default )
 		return (_wcsicmp(StrVal.c_str(), L"true") == 0) || (_wtoi(StrVal.c_str()) != 0);
 	
 	return Default;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+RegistrySettings::RegistrySettings( const wchar_t* RootKey )
+{
+	m_strRegKeyName = RootKey;
+	m_strRegKeyName.append(L"\\");
+	m_strRegKeyName.append(SETTINGS_KEY_REGISTRY);
+
+	m_hkRegKey = 0;
+}
+
+RegistrySettings::RegistrySettings( const char* RootKey )
+{
+	m_strRegKeyName = ConvertString(RootKey);
+	m_strRegKeyName.append(L"\\");
+	m_strRegKeyName.append(SETTINGS_KEY_REGISTRY);
+
+	m_hkRegKey = 0;
+}
+
+RegistrySettings::~RegistrySettings()
+{
+	if (m_hkRegKey != 0)
+	{
+		RegCloseKey(m_hkRegKey);
+		m_hkRegKey = 0;
+	}
+}
+
+bool RegistrySettings::Open(int CanWrite)
+{
+	if (m_hkRegKey != 0)
+		return true;
+	
+	LSTATUS retVal;
+	
+	if (CanWrite)
+		retVal = RegCreateKey(HKEY_CURRENT_USER, m_strRegKeyName.c_str(), &m_hkRegKey);
+	else
+		retVal = RegOpenKey(HKEY_CURRENT_USER, m_strRegKeyName.c_str(), &m_hkRegKey);
+
+	return (retVal == ERROR_SUCCESS);
+}
+
+bool RegistrySettings::GetValue( const wchar_t* ValueName, int &Output )
+{
+	int Value;
+	DWORD dwValueSize = sizeof(Value);
+
+	LSTATUS retVal = RegQueryValueEx(m_hkRegKey, ValueName, NULL, NULL, (LPBYTE) &Value, &dwValueSize);
+	if (retVal == ERROR_SUCCESS)
+	{
+		Output = Value;
+		return true;
+	}
+
+	return false;
+}
+
+bool RegistrySettings::GetValue( const wchar_t* ValueName, wchar_t *Output, size_t OutputMaxSize )
+{
+	DWORD dwValueSize = (DWORD) OutputMaxSize;
+
+	LSTATUS retVal = RegQueryValueEx(m_hkRegKey, ValueName, NULL, NULL, (LPBYTE) Output, &dwValueSize);
+	return (retVal == ERROR_SUCCESS);
+}
+
+bool RegistrySettings::GetValue( const char* ValueName, char *Output, size_t OutputMaxSize )
+{
+	DWORD dwValueSize = (DWORD) OutputMaxSize;
+
+	LSTATUS retVal = RegQueryValueExA(m_hkRegKey, ValueName, NULL, NULL, (LPBYTE) &Output, &dwValueSize);
+	return (retVal == ERROR_SUCCESS);
+}
+
+bool RegistrySettings::SetValue( const wchar_t* ValueName, int Value )
+{
+	if (!m_fCanWrite || !m_hkRegKey) return false;
+
+	LSTATUS retVal = RegSetValueEx(m_hkRegKey, ValueName, 0, REG_DWORD, (LPBYTE) &Value, sizeof(Value));
+	return (retVal == ERROR_SUCCESS);
+}
+
+bool RegistrySettings::SetValue( const wchar_t* ValueName, const wchar_t *Value )
+{
+	if (!m_fCanWrite || !m_hkRegKey) return false;
+
+	size_t nDataSize = (wcslen(Value) + 1) * sizeof(wchar_t);
+	LSTATUS retVal = RegSetValueEx(m_hkRegKey, ValueName, 0, REG_SZ, (LPBYTE) Value, (DWORD) nDataSize);
+	return (retVal == ERROR_SUCCESS);
+}
+
+bool RegistrySettings::SetValue( const char* ValueName, const char *Value )
+{
+	if (!m_fCanWrite || !m_hkRegKey) return false;
+
+	size_t nDataSize = (strlen(Value) + 1) * sizeof(char);
+	LSTATUS retVal = RegSetValueExA(m_hkRegKey, ValueName, 0, REG_SZ, (LPBYTE) Value, (DWORD) nDataSize);
+	return (retVal == ERROR_SUCCESS);
 }
--- src/common/OptionsParser.h
+++ src/common/OptionsParser.h
@@ -23,9 +23,37 @@ public:
 	bool GetValueAsBool(const wchar_t* Key, bool Default);
 	int GetValueAsInt(const wchar_t* Key, int Default);
 	wstring GetValueAsString(const wchar_t* Key, const wchar_t* Default);
+	string GetValueAsAnsiString(const wchar_t* Key, const char* Default);
 
 	const OptionsItem& operator[](size_t index) { return m_vValues.at(index); }
 	size_t NumOptions() { return m_vValues.size(); }
+};
+
+class RegistrySettings
+{
+private:
+	wstring m_strRegKeyName;
+	HKEY m_hkRegKey;
+	bool m_fCanWrite;
+
+	// Make class non-copyable
+	RegistrySettings(const RegistrySettings &other) {}
+	RegistrySettings& operator =(const RegistrySettings &other) {}
+
+public:
+	RegistrySettings(const wchar_t* RootKey);
+	RegistrySettings(const char* RootKey);
+	~RegistrySettings();
+
+	bool Open(int CanWrite = false);
+
+	bool GetValue(const wchar_t* ValueName, int &Output);
+	bool GetValue(const wchar_t* ValueName, wchar_t *Output, size_t OutputMaxSize);
+	bool GetValue(const char* ValueName, char *Output, size_t OutputMaxSize);
+
+	bool SetValue(const wchar_t* ValueName, int Value);
+	bool SetValue(const wchar_t* ValueName, const wchar_t *Value);
+	bool SetValue(const char* ValueName, const char *Value);
 };
 
 bool ParseOptions(const wchar_t* wszConfigFile, const wchar_t* wszSectionName, OptionsList &opts);
