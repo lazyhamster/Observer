249fa2339317ca594ff37e83aa31f00e9097e60f
Ariman <ariman@inbox.ru>
2011-02-23 20:11:02 +0000

    Several new stub projects.

---
+++ src/modules/mbox/mbox.cpp
@@ -0,0 +1,30 @@
+// mbox.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "ModuleDef.h"
+
+
+int MODULE_EXPORT LoadSubModule(const wchar_t* settings)
+{
+	return TRUE;
+}
+
+int MODULE_EXPORT OpenStorage(StorageOpenParams params, HANDLE *storage, StorageGeneralInfo* info)
+{
+	return FALSE;
+}
+
+void MODULE_EXPORT CloseStorage(HANDLE storage)
+{
+}
+
+int MODULE_EXPORT GetStorageItem(HANDLE storage, int item_index, LPWIN32_FIND_DATAW item_data, wchar_t* item_path, size_t path_size)
+{
+	return GET_ITEM_NOMOREITEMS;
+}
+
+int MODULE_EXPORT ExtractItem(HANDLE storage, ExtractOperationParams params)
+{
+	return SER_ERROR_SYSTEM;
+}
