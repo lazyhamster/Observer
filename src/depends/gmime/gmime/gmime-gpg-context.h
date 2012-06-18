c866a97c22d2f7b78fdefc5b7bb81f377ce00574
Ariman <ariman@inbox.ru>
2012-06-18 15:10:11 +0000

    Continue with GMime update.

---
+++ src/depends/gmime/gmime/gmime-gpg-context.h
@@ -0,0 +1,78 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*  GMime
+ *  Copyright (C) 2000-2012 Jeffrey Stedfast
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301, USA.
+ */
+
+
+#ifndef __GMIME_GPG_CONTEXT_H__
+#define __GMIME_GPG_CONTEXT_H__
+
+#include <gmime/gmime-crypto-context.h>
+
+G_BEGIN_DECLS
+
+#define GMIME_TYPE_GPG_CONTEXT            (g_mime_gpg_context_get_type ())
+#define GMIME_GPG_CONTEXT(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GMIME_TYPE_GPG_CONTEXT, GMimeGpgContext))
+#define GMIME_GPG_CONTEXT_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GMIME_TYPE_GPG_CONTEXT, GMimeGpgContextClass))
+#define GMIME_IS_GPG_CONTEXT(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GMIME_TYPE_GPG_CONTEXT))
+#define GMIME_IS_GPG_CONTEXT_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GMIME_TYPE_GPG_CONTEXT))
+#define GMIME_GPG_CONTEXT_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GMIME_TYPE_GPG_CONTEXT, GMimeGpgContextClass))
+
+typedef struct _GMimeGpgContext GMimeGpgContext;
+typedef struct _GMimeGpgContextClass GMimeGpgContextClass;
+
+
+/**
+ * GMimeGpgContext:
+ * @parent_object: parent #GMimeCryptoContext
+ * @always_trust: %TRUE if keys should always be trusted
+ * @path: path to gpg
+ *
+ * A GnuPG crypto context.
+ **/
+struct _GMimeGpgContext {
+	GMimeCryptoContext parent_object;
+	gboolean auto_key_retrieve;
+	gboolean always_trust;
+	gboolean use_agent;
+	char *path;
+};
+
+struct _GMimeGpgContextClass {
+	GMimeCryptoContextClass parent_class;
+	
+};
+
+
+GType g_mime_gpg_context_get_type (void);
+
+
+GMimeCryptoContext *g_mime_gpg_context_new (GMimePasswordRequestFunc request_passwd, const char *path);
+
+gboolean g_mime_gpg_context_get_auto_key_retrieve (GMimeGpgContext *ctx);
+void g_mime_gpg_context_set_auto_key_retrieve (GMimeGpgContext *ctx, gboolean auto_key_retrieve);
+
+gboolean g_mime_gpg_context_get_always_trust (GMimeGpgContext *ctx);
+void g_mime_gpg_context_set_always_trust (GMimeGpgContext *ctx, gboolean always_trust);
+
+gboolean g_mime_gpg_context_get_use_agent (GMimeGpgContext *ctx);
+void g_mime_gpg_context_set_use_agent (GMimeGpgContext *ctx, gboolean use_agent);
+
+G_END_DECLS
+
+#endif /* __GMIME_GPG_CONTEXT_H__ */
