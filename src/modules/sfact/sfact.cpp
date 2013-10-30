1f726d3e4add4e9f3e779311d8de11e98a9cbc86
Ariman <ariman@inbox.ru>
2013-10-30 07:55:49 +0000

    Stub module for Setup Factory installers.

---
+++ src/modules/sfact/sfact.cpp
@@ -0,0 +1,45 @@
+// sfact.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "ModuleDef.h"
+
+
+int MODULE_EXPORT OpenStorage(StorageOpenParams params, HANDLE *storage, StorageGeneralInfo* info)
+{
+	return SOR_INVALID_FILE;
+}
+
+void MODULE_EXPORT CloseStorage(HANDLE storage)
+{
+}
+
+int MODULE_EXPORT GetStorageItem(HANDLE storage, int item_index, StorageItemInfo* item_info)
+{
+	return GET_ITEM_NOMOREITEMS;
+}
+
+int MODULE_EXPORT ExtractItem(HANDLE storage, ExtractOperationParams params)
+{
+	return SER_ERROR_SYSTEM;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Exported Functions
+//////////////////////////////////////////////////////////////////////////
+
+int MODULE_EXPORT LoadSubModule(ModuleLoadParameters* LoadParams)
+{
+	LoadParams->ModuleVersion = MAKEMODULEVERSION(1, 0);
+	LoadParams->ApiVersion = ACTUAL_API_VERSION;
+	LoadParams->OpenStorage = OpenStorage;
+	LoadParams->CloseStorage = CloseStorage;
+	LoadParams->GetItem = GetStorageItem;
+	LoadParams->ExtractItem = ExtractItem;
+
+	return TRUE;
+}
+
+void MODULE_EXPORT UnloadSubModule()
+{
+}
