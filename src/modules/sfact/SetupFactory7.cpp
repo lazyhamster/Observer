b1230b7896217a70b8e948d99721fe4febbfaab5
Ariman <ariman@inbox.ru>
2014-09-16 18:17:59 +0400

    SFact: Added empty class for SF7 implementation.

---
+++ src/modules/sfact/SetupFactory7.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "SetupFactory7.h"
+
+SetupFactory7::SetupFactory7( void )
+{
+
+}
+
+SetupFactory7::~SetupFactory7( void )
+{
+
+}
+
+bool SetupFactory7::Open( CFileStream* inFile )
+{
+	return false;
+}
+
+void SetupFactory7::Close()
+{
+
+}
+
+int SetupFactory7::EnumFiles()
+{
+	return 0;
+}
+
+bool SetupFactory7::ExtractFile( int index, AStream* outStream )
+{
+	return false;
+}
---
+++ src/modules/sfact/SetupFactory7.h
@@ -0,0 +1,20 @@
+#ifndef SetupFactory7_h__
+#define SetupFactory7_h__
+
+#include "SfFile.h"
+
+class SetupFactory7 :
+	public SetupFactoryFile
+{
+private:
+public:
+	SetupFactory7(void);
+	~SetupFactory7(void);
+
+	bool Open(CFileStream* inFile);
+	void Close();
+	int EnumFiles();
+	bool ExtractFile(int index, AStream* outStream);
+};
+
+#endif // SetupFactory7_h__
