7e2f4e330a5ef2fa871b8eab10e413ff984370ad
Ariman <ariman@inbox.ru>
2016-02-10 18:54:49 +0300

    Gentee: Stub methods for old-type installers.

--- src/modules/gentee/InstallerOldFile.cpp
+++ src/modules/gentee/InstallerOldFile.cpp
@@ -1,2 +1,37 @@
 #include "stdafx.h"
 #include "InstallerOldFile.h"
+
+InstallerOldFile::InstallerOldFile()
+{
+	//
+}
+
+InstallerOldFile::~InstallerOldFile()
+{
+	//
+}
+
+bool InstallerOldFile::Open(AStream* inStream, int64_t startOffset, bool ownStream)
+{
+	return false;
+}
+
+void InstallerOldFile::Close()
+{
+	//
+}
+
+bool InstallerOldFile::GetFileDesc(int index, StorageItemInfo* desc)
+{
+	return false;
+}
+
+GenteeExtractResult InstallerOldFile::ExtractFile(int index, AStream* dest, const char* password)
+{
+	return Failure;
+}
+
+void InstallerOldFile::GetCompressionName(wchar_t* buf, size_t bufSize)
+{
+	//
+}
--- src/modules/gentee/InstallerOldFile.h
+++ src/modules/gentee/InstallerOldFile.h
@@ -5,7 +5,20 @@
 
 class InstallerOldFile : public BaseGenteeFile
 {
+public:
+	InstallerOldFile();
+	virtual ~InstallerOldFile();
 
+	virtual bool Open(AStream* inStream, int64_t startOffset, bool ownStream);
+	virtual void Close();
+
+	virtual int GetFilesCount() { return 0; }
+	virtual bool GetFileDesc(int index, StorageItemInfo* desc);
+
+	virtual GenteeExtractResult ExtractFile(int index, AStream* dest, const char* password);
+
+	virtual const wchar_t* GetFileTypeName() { return L"Gentee Installer"; }
+	virtual void GetCompressionName(wchar_t* buf, size_t bufSize);
 };
 
 #endif // InstallerOldFile_h__
