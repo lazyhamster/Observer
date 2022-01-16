41a3408d4121e21d43cc55b1c78af85c290fd77a
Roman Kharitonov <rkharito@yandex.ru>
2022-01-17 01:30:46 +1000

    Fix precompiled header source file comments

--- src/modules/wise/stdafx.cpp
+++ src/modules/wise/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// isoimg.pch will be the pre-compiled header
+// wise.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
